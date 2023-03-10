9e006253255d574825f572aad80d320c1af77665
gerry <gerritfresen4@gmail.com>
2023-03-10 22:51:41 +0100

    Make player able to heal fence

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,6 +8,11 @@ public class PlayerController : MonoBehaviour
 
     private Rigidbody2D _rigidbody;
     private float _velocity;
+    private bool _isInteracting;
+
+    public delegate void OnPlayerFenceInteraction();
+
+    public static OnPlayerFenceInteraction onPlayerFenceInteraction;
 
     private void Awake()
     {
@@ -19,8 +24,25 @@ public class PlayerController : MonoBehaviour
         _rigidbody.velocity = new Vector2(_velocity * (Time.deltaTime * moveSpeed), 0f);
     }
 
+    private void Update()
+    {
+        Debug.Log((_isInteracting));
+    }
+
     public void OnMove(InputValue value)
     {
         _velocity = value.Get<float>();
+    }
+
+    public void OnInteract(InputValue value)
+    {
+        _isInteracting = value.Get<float>() > 0f;
+    }
+
+    public void OnTriggerStay2D(Collider2D other)
+    {
+        // TODO Add cooldown for Player/Fence interaction
+        if (!_isInteracting || !other.gameObject.layer.Equals(LayerMask.NameToLayer("Fence"))) return;
+        onPlayerFenceInteraction?.Invoke();
     }
 }
