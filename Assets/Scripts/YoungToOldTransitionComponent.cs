e8cf08852bd104312c7e50b4b722d4f8689c1b38
gerry <gerritfresen4@gmail.com>
2023-03-14 21:40:42 +0100

    Restructure young/old transition value handling

---
+++ Assets/Scripts/YoungToOldTransitionComponent.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+
+public class YoungToOldTransitionComponent : MonoBehaviour
+{
+    [SerializeField] private int remainingYearsForYoung;
+    [SerializeField] private int remainingYearsForOld;
+
+    private DataHandlerComponent _dataHandlerComponent;
+
+    public delegate void YoungOldTransitionChanged(float newValue);
+
+    public static YoungOldTransitionChanged OnYoungOldTransitionChanged;
+
+    private void Start()
+    {
+        DataHandlerComponent.OnRemainingYearsChanged += OnRemainingYearsChanged;
+    }
+
+    private void OnRemainingYearsChanged(int remainingYears)
+    {
+        float transitionValue;
+        if (remainingYears > remainingYearsForYoung)
+        {
+            transitionValue = 1f;
+        }
+        else if (remainingYears > remainingYearsForOld)
+        {
+            transitionValue = (remainingYears - remainingYearsForOld) /
+                              (float)(remainingYearsForYoung - remainingYearsForOld);
+        }
+        else
+        {
+            transitionValue = 0f;
+        }
+
+        Debug.Log(transitionValue);
+        OnYoungOldTransitionChanged?.Invoke(transitionValue);
+    }
+}
