3c05341c742b9ce5058b838ad5c2e102bc27bf23
gerry <gerritfresen4@gmail.com>
2023-03-15 18:51:35 +0100

    Add Player Idle + Moving animation

--- Assets/Scripts/PlayerSpriteComponent.cs
+++ Assets/Scripts/PlayerSpriteComponent.cs
@@ -3,38 +3,20 @@ using UnityEngine;
 [RequireComponent(typeof(SpriteRenderer))]
 public class PlayerSpriteComponent : MonoBehaviour
 {
-    [SerializeField] private int remainingYearsForYoung;
-    [SerializeField] private int remainingYearsForOld;
     [SerializeField] private SpriteRenderer oldSpriteRenderer;
 
     private SpriteRenderer _renderer;
-    private DataHandlerComponent _dataHandlerComponent;
 
     private void Start()
     {
-        PlayerController.OnPlayerMove += HandleHorizontalFlip;
         YoungToOldTransitionComponent.OnYoungOldTransitionChanged += OnYoungOldTransitionChanged;
 
         _renderer = GetComponent<SpriteRenderer>();
-        _dataHandlerComponent = GameObject.FindWithTag("DataHandler").GetComponent<DataHandlerComponent>();
     }
 
     private void OnYoungOldTransitionChanged(float newValue)
     {
         _renderer.color = new Color(1f, 1f, 1f, newValue);
         oldSpriteRenderer.color = new Color(1f, 1f, 1f, 1f - newValue);
-    }
-
-    private void HandleHorizontalFlip(float xVelocity)
-    {
-        if (xVelocity < 0f)
-        {
-            _renderer.flipX = false;
-        }
-
-        if (xVelocity > 0f)
-        {
-            _renderer.flipX = true;
-        }
     }
 }
