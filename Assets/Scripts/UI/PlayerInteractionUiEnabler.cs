0c4b5e1f11a183dc1c595a67d115982c82eb567e
gerry <gerritfresen4@gmail.com>
2023-07-30 22:46:54 +0200

    make PlayerInteractionUiEnabler more flexible

--- Assets/Scripts/UI/PlayerInteractionUiEnabler.cs
+++ Assets/Scripts/UI/PlayerInteractionUiEnabler.cs
@@ -1,65 +1,82 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 public class PlayerInteractionUiEnabler : MonoBehaviour
 {
-    [SerializeField] private GameObject fenceUiObject;
-    [SerializeField] private GameObject treeUiObject;
-    [SerializeField] private GameObject mineUiObject;
-    [SerializeField] private GameObject statueUiObject;
+    [System.Serializable]
+    public struct ObjectLayer
+    {
+        public string layer;
+        public GameObject gameObject;
+    }
+
+    [SerializeField] private List<ObjectLayer> uiObjects;
 
     private void OnTriggerStay2D(Collider2D other)
     {
-        SetActiveFoLabels(LayerMask.LayerToName(other.gameObject.layer), true);
+        foreach (var obj in uiObjects)
+        {
+            obj.gameObject.SetActive(obj.layer == 
+            LayerMask.LayerToName(other.gameObject.layer));
+        }
     }
 
     private void OnTriggerExit2D(Collider2D other)
     {
-        string layer = LayerMask.LayerToName(other.gameObject.layer);
-        if (layer == "FenceTrigger" || layer == "Tree" 
-        || layer == "Stone" || layer == "Statue")
+        bool interactableExited = false;
+        foreach (var obj in uiObjects)
         {
-            statueUiObject.SetActive(false);
-            treeUiObject.SetActive(false);
-            mineUiObject.SetActive(false);
-            fenceUiObject.SetActive(false);
+            if (LayerMask.LayerToName(other.gameObject.layer) == obj.layer)
+            {
+                interactableExited = true;
+                break;
+            }
+        }
+
+        if (interactableExited)
+        {
+            foreach (var obj in uiObjects)
+            {
+                obj.gameObject.SetActive(false);
+            }
         }
     }
 
-    private void SetActiveFoLabels(string layer, bool value)
-    {
-        switch(layer)
-        {
-            case "FenceTrigger":
-                fenceUiObject.SetActive(value);
-                treeUiObject.SetActive(!value);
-                mineUiObject.SetActive(!value);
-                statueUiObject.SetActive(!value);
-                break;
-            case "Tree":
-                treeUiObject.SetActive(value);
-                fenceUiObject.SetActive(!value);
-                mineUiObject.SetActive(!value);
-                statueUiObject.SetActive(!value);
-                break;
-            case "Stone":
-                mineUiObject.SetActive(value);
-                treeUiObject.SetActive(!value);
-                fenceUiObject.SetActive(!value);
-                statueUiObject.SetActive(!value);
-                break;
-            case "Statue":
-                statueUiObject.SetActive(value);
-                treeUiObject.SetActive(!value);
-                mineUiObject.SetActive(!value);
-                fenceUiObject.SetActive(!value);
-                break;
-            default:
-                statueUiObject.SetActive(!value);
-                treeUiObject.SetActive(!value);
-                mineUiObject.SetActive(!value);
-                fenceUiObject.SetActive(!value);
-                break;
+    //private void SetActiveFoLabels(string layer, bool value)
+    //{
+    //    switch(layer)
+    //    {
+    //        case "FenceTrigger":
+    //            fenceUiObject.SetActive(value);
+    //            treeUiObject.SetActive(!value);
+    //            mineUiObject.SetActive(!value);
+    //            statueUiObject.SetActive(!value);
+    //            break;
+    //        case "Tree":
+    //            treeUiObject.SetActive(value);
+    //            fenceUiObject.SetActive(!value);
+    //            mineUiObject.SetActive(!value);
+    //            statueUiObject.SetActive(!value);
+    //            break;
+    //        case "Stone":
+    //            mineUiObject.SetActive(value);
+    //            treeUiObject.SetActive(!value);
+    //            fenceUiObject.SetActive(!value);
+    //            statueUiObject.SetActive(!value);
+    //            break;
+    //        case "Statue":
+    //            statueUiObject.SetActive(value);
+    //            treeUiObject.SetActive(!value);
+    //            mineUiObject.SetActive(!value);
+    //            fenceUiObject.SetActive(!value);
+    //            break;
+    //        default:
+    //            statueUiObject.SetActive(!value);
+    //            treeUiObject.SetActive(!value);
+    //            mineUiObject.SetActive(!value);
+    //            fenceUiObject.SetActive(!value);
+    //            break;
 
-        }
-    }
+    //    }
+    //}
 }
