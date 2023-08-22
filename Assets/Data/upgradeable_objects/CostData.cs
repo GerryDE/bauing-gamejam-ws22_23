4d2688edb593aeede522d0545f13ed1efc1317bf
gerry <gerritfresen4@gmail.com>
2023-08-22 23:20:36 +0200

    add cost data objects

---
+++ Assets/Data/upgradeable_objects/CostData.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace Data
+{
+    [CreateAssetMenu(fileName = "Assets/Data/upgradeable_objects/CostData", menuName = "Data/CostData")]
+    public class CostData : ScriptableObject
+    {
+        public int lumberCost;
+        public int stoneCost;
+    }
+}
