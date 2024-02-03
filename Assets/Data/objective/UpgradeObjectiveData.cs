68494d1c1ca0829cf057ce9f402c140428a27925
GerryDE <gerritfresen4@gmail.com>
2024-02-03 11:23:07 +0100

    Improve Tutorial Upgrade text

--- Assets/Data/objective/UpgradeObjectiveData.cs
+++ Assets/Data/objective/UpgradeObjectiveData.cs
@@ -7,7 +7,9 @@ namespace Data.objective
     {
         public override string GetObjectiveText()
         {
-            return "Perform an upgrade";
+            return "Perform an upgrade. Upgrading the fence ensures more protection.\n"
+                   + "Upgrading trees or mine makes you farm more resources.\n"
+                   + "Upgrading the statue raises the Player's stats.";
         }
     }
 }
