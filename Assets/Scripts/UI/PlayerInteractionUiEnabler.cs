4ac2d9298db8bcda114ce9820daa161818bfc94a
GerryDE <gerritfresen4@gmail.com>
2024-03-02 23:19:23 +0100

    UI improvements

--- Assets/Scripts/UI/PlayerInteractionUiEnabler.cs
+++ Assets/Scripts/UI/PlayerInteractionUiEnabler.cs
@@ -1,43 +1,46 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerInteractionUiEnabler : MonoBehaviour
+namespace UI
 {
-    [System.Serializable]
-    public struct ObjectLayer
+    public class PlayerInteractionUiEnabler : MonoBehaviour
     {
-        public string layer;
-        public GameObject gameObject;
-    }
-
-    [SerializeField] private List<ObjectLayer> uiObjects;
-
-    private void OnTriggerStay2D(Collider2D other)
-    {
-        foreach (var obj in uiObjects)
+        [System.Serializable]
+        public struct ObjectLayer
         {
-            obj.gameObject.SetActive(obj.layer == 
-            LayerMask.LayerToName(other.gameObject.layer));
-        }
-    }
-
-    private void OnTriggerExit2D(Collider2D other)
-    {
-        bool interactableExited = false;
-        foreach (var obj in uiObjects)
-        {
-            if (LayerMask.LayerToName(other.gameObject.layer) == obj.layer)
-            {
-                interactableExited = true;
-                break;
-            }
+            public string layer;
+            public GameObject gameObject;
         }
 
-        if (interactableExited)
+        [SerializeField] private List<ObjectLayer> uiObjects;
+
+        private void OnTriggerStay2D(Collider2D other)
         {
             foreach (var obj in uiObjects)
             {
-                obj.gameObject.SetActive(false);
+                obj.gameObject.SetActive(obj.layer == 
+                                         LayerMask.LayerToName(other.gameObject.layer));
+            }
+        }
+
+        private void OnTriggerExit2D(Collider2D other)
+        {
+            bool interactableExited = false;
+            foreach (var obj in uiObjects)
+            {
+                if (LayerMask.LayerToName(other.gameObject.layer) == obj.layer)
+                {
+                    interactableExited = true;
+                    break;
+                }
+            }
+
+            if (interactableExited)
+            {
+                foreach (var obj in uiObjects)
+                {
+                    obj.gameObject.SetActive(false);
+                }
             }
         }
     }
