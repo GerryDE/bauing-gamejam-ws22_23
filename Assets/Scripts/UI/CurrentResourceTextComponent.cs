ce549d2681afbbfd57e5c477ffcc95985a3db584
GerryDE <gerritfresen4@gmail.com>
2024-09-04 22:17:50 +0200

    Add static resource UI

---
+++ Assets/Scripts/UI/CurrentResourceTextComponent.cs
@@ -0,0 +1,32 @@
+using System;
+using TMPro;
+using UnityEngine;
+
+namespace UI
+{
+    [RequireComponent(typeof(TextMeshProUGUI))]
+    public class CurrentResourceTextComponent : MonoBehaviour
+    {
+        private enum ResourceType { Lumber, Stone };
+
+        [SerializeField] private ResourceType resourceType;
+
+        private TextMeshProUGUI _textComponent;
+
+        public void Start() 
+        {
+            _textComponent = GetComponent<TextMeshProUGUI>();
+
+            DataProvider.OnResourceDataChanged += OnResourceDataChanged;
+        }
+
+        private void OnResourceDataChanged(DataProvider.CurrentResourceData data)
+        {
+            if (resourceType == ResourceType.Lumber) {
+                _textComponent.text = data.WoodAmount.ToString();
+            } else if (resourceType == ResourceType.Stone) {
+                _textComponent.text = data.StoneAmount.ToString();
+            }
+        }
+    }
+}
